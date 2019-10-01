4fc3f161a209e01b13bc5998a05d380afa861340
Aixi <friendgic@gmail.com>
2019-10-01 14:46:55 +0200

    2

--- mypage.js
+++ mypage.js
@@ -1,7 +1,8 @@
 $('#Submit').button().click(function () {
     var cmd = $("#CmdInput").val() 
     console.log(cmd);
-    $.post("http://127.0.0.1:1200/CMD", {CMD:cmd}, function (result) {
+    var url =window.location.href ;
+    $.post(url+"CMD", {CMD:cmd}, function (result) {
         alert(result.ACK?'Successful':'Fail')
         if(result.dat!=undefined){  
             $("#myTable").empty()
