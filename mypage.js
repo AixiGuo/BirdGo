47532e0756b3e3f763d44d407e780e103b5d9105
Aixi <friendgic@gmail.com>
2019-10-01 14:42:18 +0200

    delete

--- mypage.js
+++
@@ -1,8 +0,0 @@
-$('#Submit').button().click(function () {
-    var cmd = $("#CmdInput").val() 
-    console.log(cmd);
-    $.post("http://127.0.0.1:1200/CMD", {CMD:cmd}, function (result) {
-        alert(result.ACK?'Successful':'Fail')
-    });
-
-})
